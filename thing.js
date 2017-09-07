b0540c41331cc2d2e49e688e3b38b3ddaf20b4d4
chasedeger <cdeger88@gmail.com>
2017-09-07 14:35:27 -0700

    updating remote repo

---
+++ thing.js
@@ -0,0 +1,11 @@
+var arr = [1,2,3,4,5,6,7,8,9,10]
+
+function search(num){
+	for(var i = 0; i<arr.length; i++){
+		if(arr[i]===num){
+			}else if (arr[i]>arr.length){
+				console.log("it aint there")
+			}
+		}
+	}
+}
